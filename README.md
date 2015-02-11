9f9a31429147732a5d4287259ff434966e8e026a
dbjohansson <denice.b.johansson1@gmail.com>
2015-02-11 23:56:00 +0100

    ex05

---
+++ README.md
@@ -0,0 +1,3 @@
+# example05
+
+A minimal command-line application.
---
+++ test/all.dart
@@ -0,0 +1,10 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+library all_tests;
+
+import 'example05_test.dart' as example05_test;
+
+void main() {
+  example05_test.defineTests();
+}
